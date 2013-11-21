1e792424c895e8715539b6c2e63d701b626d17c2
equii <theequimanthorn@gmail.com>
2013-11-21 23:56:34 +0200

    tests libs added + 2 tests for authentication

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ var express = require('express')
 
 var app = express();
  
+ module.exports = app;
 
 // all environments
 app.set('port', process.env.PORT || 3000);
---
+++ test/auth.js
@@ -0,0 +1,46 @@
+'use strict';
+
+var app = require('../app');
+var should = require('should');
+var request = require('supertest');
+
+describe('Passport: authentication', function(){
+
+	it('should redirect to /login if not authenticated', function(done){
+		var post ={
+			username: 'fake',
+			password: 'fake'
+		};
+
+		request(app)
+		.post('/login')
+		.send(post)
+		.expect(302)
+		.end(function(err, res){
+			should.not.exist(err);
+
+			res.header.location.should.include('login');
+			done();
+		});
+	});
+
+	it('should redirect to / if auth successfully', function(done){
+		var post = {
+			username: 'test',
+			password: 'test'
+		};
+
+		request(app)
+		.post('/login')
+		.send(post)
+		.end(function(err,res){
+				should.not.exist(err);
+
+
+				res.header.location.should.not.include('login');
+				res.header.location.should.include('/');
+				done();
+		});
+	});
+
+});
